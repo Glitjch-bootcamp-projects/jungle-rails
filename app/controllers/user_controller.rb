e2253bda898b1fe34c9f9f21b314c88dca3b412a
glitjch <t.jamesphan@gmail.com>
2022-03-10 23:56:38 +0000

    updated user model with has_secure_password

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,5 +3,22 @@ class UserController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+    else
+      redirect_to '/'
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(
+        :email,
+        :name,
+        :password,
+        :password_confirmation
+      )
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  
+  # gives our User model authentication methods via bcrypt
+  has_secure_password
 end
