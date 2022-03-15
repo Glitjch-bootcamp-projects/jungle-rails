690dcab2f60483dafbca6dde7d5ab1ecd6c72086
glitjch <t.jamesphan@gmail.com>
2022-03-15 00:21:14 +0000

    changed schema to replace name with first name and last name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+
+#   It must be created with a password and password_confirmation fields
+# These need to match so you should have an example for where they are not the same
+# These are required when creating the model so you should also have an example for this
+# Emails must be unique (not case sensitive; for example, TEST@TEST.com should not be allowed if test@test.COM is in the database)
+# Email, first name, and last name should also be required
 end
