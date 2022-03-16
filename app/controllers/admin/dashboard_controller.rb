41fa6af7e8f5e692f692f7f367337cc5947a60d9
glitjch <t.jamesphan@gmail.com>
2022-03-16 05:47:43 +0000

    refactored admin authentication to be required for any admin page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < ApplicationController
   # uncomment this below to activate a user/password access only
   # before_filter :authorize
+  http_basic_authenticate_with name: ENV['username'], password: ENV['password']
+
 
   def show
     @categories = Category.all
